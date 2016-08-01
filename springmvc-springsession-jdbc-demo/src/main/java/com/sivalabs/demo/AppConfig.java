4772bb9a419f0496f71f2e8b42a141868a93d3ee
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2016-08-01 18:02:42 +0530

    SpringMVC Spring Session Jdbc Demo

---
+++ springmvc-springsession-jdbc-demo/src/main/java/com/sivalabs/demo/AppConfig.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.sivalabs.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Siva
+ *
+ */
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = { "classpath:application.properties" })
+@EnableJdbcHttpSession
+public class AppConfig {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer()
+	{
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	@Value("${init-db:false}")
+	private String initDatabase;
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource dataSource)
+	{
+		return new DataSourceTransactionManager(dataSource);
+	}
+	
+	@Bean
+	public DataSource dataSource()
+	{
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		return dataSource;
+	}
+	
+	@Bean
+	public DataSourceInitializer dataSourceInitializer(DataSource dataSource) 
+	{
+		DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+		dataSourceInitializer.setDataSource(dataSource);
+		dataSourceInitializer.setEnabled(env.getProperty("init-db", Boolean.class, false));
+
+		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+		dataSourceInitializer.setDatabasePopulator(databasePopulator);
+
+		String dbType = env.getProperty("dbType","h2");
+		String ss_schema = "org/springframework/session/jdbc/schema-"+dbType+".sql";
+		databasePopulator.addScript(new ClassPathResource(ss_schema));
+		
+		//If the Spring Session tables are already exists
+		databasePopulator.setContinueOnError(true);
+		
+		return dataSourceInitializer;
+	}
+}
---
+++ springmvc-springsession-jdbc-demo/src/main/java/com/sivalabs/demo/SpringSessionInitializer.java
@@ -0,0 +1,13 @@
+
+package com.sivalabs.demo;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class SpringSessionInitializer extends AbstractHttpSessionApplicationInitializer 
+{
+
+	public SpringSessionInitializer() 
+	{
+		//super(AppConfig.class);
+	}
+}
