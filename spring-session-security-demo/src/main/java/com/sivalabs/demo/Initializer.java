6993ad2fd3951b7f7ac332e7386e47ac1f847d89
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2018-02-07 09:48:26 +0530

    Spring Session samples upgraded to 2.0 version.

--- spring-session-security-demo/src/main/java/com/sivalabs/demo/Initializer.java
+++
@@ -1,9 +0,0 @@
-
-package com.sivalabs.demo;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class Initializer extends AbstractHttpSessionApplicationInitializer {
-
-}
-
